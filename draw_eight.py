9618d7c2edd77dc20a13a6c2f8146894be3a3a7d
Sara Pozas <Sara@MacBook-Air.local>
2024-10-13 20:07:59 +0100

    Completed prework for week 3

--- draw_eight.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-def make_figure():
-    theta = np.linspace(0, 2 * np.pi, 100)
-    fig, ax = plt.subplots(figsize=(3, 6))
-    ax.plot(np.sin(2 * theta) / 2, np.cos(theta))
-    return fig
--- test.py
+++
@@ -1,7 +0,0 @@
-class Room():
-    pass
-
-myroom = Room()
-myroom.name = "Ari's room"
-
-print(dir(myroom))
